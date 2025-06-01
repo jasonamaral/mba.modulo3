76ee2cb91daee19634e42fffc73266e87ab48bbf
DARKLEGION\Jason <jason.amaral@gmail.com>
2025-06-01 11:26:48 -0300

    Correção
    Adiciona nova propriedade 'IsActive' ao modelo de migração do ContentDbContext para gerenciar o estado ativo dos conteúdos. Atualiza a estrutura do banco de dados para refletir essa mudança.

--- src/BoundedContexts/ContentManagement/Infrastructure/Migrations/20250601141237_AddIsActiveToLessons.Designer.cs
+++ src/BoundedContexts/ContentManagement/Infrastructure/Migrations/20250601141237_AddIsActiveToLessons.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FluencyHub.ContentManagement.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FluencyHub.ContentManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ContentDbContext))]
-    partial class ContentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601141237_AddIsActiveToLessons")]
+    partial class AddIsActiveToLessons
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.16");
